64e012c0faafadf15b52ba0c0ef5744fce2459ac
jonathanwlopes <jonathan@agenciaeplus.com.br>
2022-05-29 17:57:52 -0300

    challenge completion

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const currentUser = this.usersRepository.findById(user_id);
+
+    if (!currentUser.admin) throw new Error("Access Denied");
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.status(200).send(user);
+    } catch (e) {
+      return response.status(404).send({ error: e.message });
+    }
   }
 }
 
